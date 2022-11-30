a53cbc6510b81e76e08bcb3fe8617a8ecdbde426
zihua_tang <zihua_tang@jsxzkingdee.com>
2022-11-30 14:37:02 +0800

    add config file

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	fmt.Println("init....")
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World.")
+	var port = viper.GetString("server.port")
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	router.Run(port)
 }
